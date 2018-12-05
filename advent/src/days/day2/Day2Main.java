5968d1c19ce6ab5e5030409258d4c989ca962874
Dejan <dejan.naric@gmail.com>
2018-12-05 14:48:34 +0100

    day 2 naloga 2 complete

--- advent/src/days/day2/Day2Main.java
+++ advent/src/days/day2/Day2Main.java
@@ -3,12 +3,12 @@ package days.day2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Day2Main {
     public static void main(String args[]) {
         calculateFrequency();
+        countBoxes();
     }
 
     private static void calculateFrequency() {
@@ -51,4 +51,60 @@ public class Day2Main {
             e.printStackTrace();
         }
     }
+
+    private static void countBoxes() {
+        try {
+            FileReader fileReader = new FileReader("advent/src/days/day2/input.txt");
+            BufferedReader br = new BufferedReader(fileReader);
+
+            String line;
+            LinkedList<String> listOfWords = new LinkedList<String>();
+            while ((line = br.readLine()) != null) {
+               listOfWords.add(line);
+            }
+            int common = 0;
+            for (int i = 0; i < listOfWords.size(); i++) {
+                List<String> listCopy = listOfWords;
+                listCopy.remove(i);
+                common += checkOthers(listOfWords.get(i), listCopy);
+            }
+
+            System.out.println(common);
+            fileReader.close();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int checkOthers(String word, List<String> listOfWords) {
+        int common = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < listOfWords.size(); i++) {
+            final String s = listOfWords.get(i);
+            String tmpWord = "";
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                char wordChar = word.charAt(j);
+                if (wordChar == c) {
+                    tmpWord += wordChar;
+                }
+            }
+            int tmpLength = tmpWord.length();
+            if(map.containsKey(tmpLength)) {
+                Integer value = map.get(tmpLength);
+                map.put(tmpLength, value+1);
+            } else {
+                map.put(tmpLength, 1);
+            }
+        }
+        Integer finalInt = map.get(word.length());
+        if(finalInt != null) {
+            common = map.get(word.length());
+        }
+        return common;
+    }
+
+
+
 }
